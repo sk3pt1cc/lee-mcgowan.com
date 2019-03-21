a7a161e2fd28afa46d3ae5750a3b89da76def552
sk3pt1cc <lee.mcgowan.work@gmail.com>
2019-03-21 22:42:36 +0000

    hahahah

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import sheetrock from "sheetrock";
+import rp from 'request-promise';
 
 import HomeWrapper from "./HomeWrapper";
 import AboutMe from "../../components/about-me";
@@ -7,37 +7,15 @@ import Container from "../../components/styled/container";
 import Blog from "../../components/blog";
 import PostDetail from "../../components/post-detail";
 
-const parseSheetsContent = response => {
-  const { rows } = response;
-  const posts = rows.map((row, idx) => {
-    const output = {};
-    const { labels, cellsArray } = row;
-
-    labels.forEach((label, idx) => {
-      output[label.toLowerCase()] = cellsArray[idx];
-    });
-
-    return output;
-  });
-
-  return posts;
-};
-
 const Home = () => {
   const [selectedPost, setSelectedPost] = useState(null);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    if (posts.length <= 0) {
-      sheetrock({
-        url: "https://docs.google.com/spreadsheets/d/1LvEMfH1CGgj3UaedMRrQbDUCOdFmOkUmXLSbrpsaSrg/edit#gid=0",
-        callback: (err, opt, response) => {
-          const res = response;
-          const posts = parseSheetsContent(res);
-          setPosts(posts.splice(1));
-        },
-      });
-    }
+    rp('/.netlify/functions/test').then((data) => {
+      console.log(data);
+      console.log('hahahaha');
+    });
   });
 
   return (
