9858a4c3f50d9f7be5cb5a49c281f40adfa9e510
sk3pt1cc <lee.mcgowan.work@gmail.com>
2019-03-22 00:03:44 +0000

    adding url prexic

--- src/app/services/http.js
+++ src/app/services/http.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// change to this pre-deploy '/.netlify/functions'
 const urlPrefix = 'http://localhost:9000';
 
 export const get = url => axios.get(`${urlPrefix}/${url}`);
